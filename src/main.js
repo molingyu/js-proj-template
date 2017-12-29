35346dc8df995a08a2b4a7bbad56f0050c7cc80a
shitake <z1522716486@hotmail.com>
2017-12-29 01:10:04 -0800

    build: init base config!

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/**
+ * return numbers sum.
+ * @param {Number} a
+ * @param {Number} b
+ */
+export default function sum (a, b) {
+  return a + b
+}
---
+++ test/test.js
@@ -0,0 +1,6 @@
+/* eslint-env jest */
+import sum from '../src/main'
+
+it('test sum', function () {
+  expect(sum(1, 2)).toBe(3)
+})
